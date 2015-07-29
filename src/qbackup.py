fdbd26bfe7e9130e27c105b19de9b58c947b007c
nykh <nicholas.ykhuang@gmail.com>
2015-07-29 14:45:57 +0800

    Traverse local drive and filter files to be downloaded
    I use pathlib, os to traverse local drive, find out which files need to be downloaded. I also compare the timestamps of remote and local versions, and download it if the remote version has a later timestamp. However, Qiniu seems to have made a mistake on their API page saying the unit of their timestamp is 100ns. The correct unit seems to be 1us.

--- src/qbackup.py
+++ src/qbackup.py
@@ -1,6 +1,8 @@
 __author__ = 'nykh'
 
 import requests as req
+import pathlib
+import os
 import qiniu
 from qiniu import BucketManager
 
@@ -8,27 +10,76 @@ from qiniu import BucketManager
 def list_bucket(bucketname):
     """
     traverse the buckt through the Qiniu API and return a list of keys
+    and timestamp
     :param bucketname:
     :return: a list of keys to download
     """
     key_list = []
     done = False
     marker = None
+    BATCH_LIMIT = 10   # maybe optimized under real condition
 
     access_key, secret_key = get_authentication()
     q = qiniu.Auth(access_key, secret_key)
     bucket = BucketManager(q)
 
     while not done:
-        ret, done, info = bucket.list(bucketname, marker=marker, limit=10)
+        ret, done, info = bucket.list(bucketname, marker=marker,
+                                      limit=BATCH_LIMIT)
         marker = ret.get('marker')
         for item in ret['items']:
-            key_list.append(item['key'])
+            key_list.append((item['key'], item['putTime']))
 
     return key_list
 
 
-def batch_download(bucketurl, keylist, output_policy=lambda x: x):
+def compare_local_and_remote(remote_key_and_ts, basepath):
+    '''
+    Compare the local files and remote list of items,
+    produce a tuple of two lists, one lists files on remote
+    not in local drive (to be downloaded), the other lists files
+    that exist on the local drive but not on remote (to be uploaded).
+    :param remote_key_and_ts:
+    :param basepath:
+    :rtype: object
+    :return: ([files to be downloaded], [files to be uploaded])
+    '''
+
+    def compare_timestamp(remote, local):
+        '''
+        on Qiniu cloud, file timestamp are with unit of 100 ns (epoch),
+        locally however the timestamp is in sec (epoch) and float number.
+        Considering the program is expected to run weekly, the comparison
+        may well be done in the unit of second. I consider collision to be
+        highly unlikely.
+
+        Problem: it seems in fact Qiniu timestamp is in the unit of 1us,
+        not 100ns. I take liberty and use 10e6 instead of 10e7 as the ratio.
+        '''
+        remote /= int(10e6)
+        local = int(local)
+        if remote > local: return 1
+        elif remote < local: return -1
+        else: return 0
+
+
+    downloadend = []
+    uploadend = []
+    for key, ts in remote_key_and_ts:
+        localpath = basepath / key
+        if not localpath.exists():
+            downloadend.append(key)
+        else:
+            cmp = compare_timestamp(ts, localpath.stat().st_mtime)
+            if cmp > 0:  # remote > local
+                downloadend.append(key)
+            elif cmp < 0:
+                uploadend.append(key)
+
+    return (downloadend, uploadend)
+
+
+def batch_download(bucketurl, keylist, basepath, output_policy=lambda x: x, verbose=False):
     '''
     side-effect: batch download list of resources from qiniu bucket
     :except: raises exception if any of the request return an error
@@ -42,8 +93,9 @@ def batch_download(bucketurl, keylist, output_policy=lambda x: x):
         res = req.get(bucketurl + key)
         if res.status_code != 200:
             raise Exception
-        with open(output_policy(key), 'wb') as of:
+        with open(str(basepath / output_policy(key)), 'wb') as of:
             of.write(res.content)
+        if verbose: print('downloaded: ' + key)
 
 
 def get_authentication():
@@ -62,7 +114,13 @@ if __name__ == '__main__':
     # hardcoded now, customizable later
     bucketname = 'llcetest'
     bucketurl = 'http://7xkpk9.com1.z0.glb.clouddn.com/'
+    basepath = pathlib.Path('/home/nykh/workspace/qiniu-backup')
+    verbose = False
 
-    key_list = list_bucket(bucketname)
+    if not basepath.exists():
+        basepath.mkdir()
+    assert basepath.exists()
 
-    batch_download(bucketurl, key_list)
+    key_and_timestamp = list_bucket(bucketname)
+    down, up = compare_local_and_remote(key_and_timestamp, basepath)
+    batch_download(bucketurl, down, basepath, verbose=verbose)
